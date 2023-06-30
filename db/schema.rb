8a8d4674365161bd6f00649a7ee606abc9f8905f
Mateus Figueiredo <mateusmelosf@gmail.com>
2023-06-30 18:39:06 -0300

    create cart resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_28_160347) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_30_212945) do
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.string "image_url"
