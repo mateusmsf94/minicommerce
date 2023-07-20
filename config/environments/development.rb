8dbfa45ebb71412c5bc054f0326055bc3b8fd017
Mateus Figueiredo <mateusmelosf@gmail.com>
2023-07-20 10:48:43 -0300

    Implemented email notifications and updated line item display
    Changes:
    - Added OrderMailer to OrdersController to send a confirmation email when an order is created.
    - Updated line items view to better display the quantity and title of the product.
    - Configured Action Mailer settings for the development environment.
    - Removed system tests for Cart, LineItems, and Products.
    - Updated Orders system test to check the presence of dynamic payment type fields.
    
    Note: Email username and password are hardcoded for development. Remember to change for production use.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,5 +66,15 @@ Rails.application.configure do
   # config.action_view.annotate_rendered_view_with_filenames = true
 
   # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'domain.of.sender.net',
+    user_name:            'dave',
+    password:             'thomas',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
