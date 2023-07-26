846140c1eccfdadd806024d19517b718528b738e
Mateus Figueiredo <mateusmelosf@gmail.com>
2023-07-26 20:12:17 -0300

    Add Locale Selection and Internationalization
    This commit enhances user experience by offering the ability to select a preferred language for the application interface. It also provides internationalization for various elements of the user interface.
    
    Key Changes:
    - Modified the StoreController to redirect to the store index with the selected locale.
    - Added a form in the application layout for language selection.
    - Adjusted the navigation links and page titles to use localized text.
    - Updated store index and product labels to use localized text.
    - Added English and Spanish translations for several UI elements.
    
    This will ensure the application can cater to a multilingual user base, providing a more inclusive and user-friendly interface.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,7 +4,11 @@ class StoreController < ApplicationController
   include CurrentCart
   before_action :set_cart
   def index
-    @products = Product.order(:title)
+    if params[:set_locale]
+      redirect_to store_index_url(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+    end
 
     if session[:counter].nil?
       session[:counter] = 1
