49c2a801dfa817fbde2e2c5b878869c042cbcc9c
Mateus Figueiredo <mateusmelosf@gmail.com>
2023-07-26 18:48:40 -0300

    Implement Internationalization Support and Update Logout Mechanism
    This commit introduces a series of changes to implement internationalization (I18n) support and update the logout mechanism for improved user experience and semantic clarity.
    
    Key changes include:
    - Added a before action in ApplicationController to set the I18n locale from URL parameters.
    - Updated the 'Logout' link to use 'button_to' instead of 'link_to' in the application layout.
    - Introduced a new initializer for I18n, setting the default locale and defining the supported languages.
    - Adjusted the routing to support locale scope and added an action 'who_bought' to the products resources.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'admin' => 'admin#index'
+
   controller :sessions do
     get 'login' => :new
     post 'login' => :create
@@ -8,14 +9,17 @@ Rails.application.routes.draw do
 
   get 'sessions/create'
   get 'sessions/destroy'
-  resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
-  resources :products
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users
+
+  resources :products do
+    get :who_bought, on: :member
+  end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
 end
